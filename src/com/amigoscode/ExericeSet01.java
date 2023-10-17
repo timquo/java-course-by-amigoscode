8f6c316f2cfa4d6ff2c5834ae4e92d7b2bd778e2
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-17 19:34:51 +0300

    set up placeholder files for future coding exercises

---
+++ src/com/amigoscode/ExericeSet01.java
@@ -0,0 +1,4 @@
+package com.amigoscode;
+
+public class ExericeSet01 {
+}
---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,13 @@
+package com.amigoscode;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+    // https://docs.oracle.com/en/java/javase/17/docs/api/index.html
+        // write your code here
+        int[] numbers = new int[3];
+        System.out.println(Arrays.toString(numbers));
+    }
+}
