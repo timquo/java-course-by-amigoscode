a7716a8c6ac380680eccdc0710968aa9c75c0599
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-18 13:30:12 +0300

    update Main.java to run through Exercise02.java

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -19,5 +19,21 @@ public class Main {
         Exercise01.task04();
         Exercise01.task05();
         Exercise01.task06();
+
+        Exercise02.task01();
+        Exercise02.task02();
+        Exercise02.task03();
+        Exercise02.task04();
+        Exercise02.task05();
+        Exercise02.task06();
+        Exercise02.task07();
+        Exercise02.task08();
+        Exercise02.task09();
+        Exercise02.task10();
+        Exercise02.task11();
+        Exercise02.task12();
+        Exercise02.task13();
+        Exercise02.task14();
+        Exercise02.task15();
     }
 }
