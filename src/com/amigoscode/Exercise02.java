8882d8c7f81e72a528b9dd40e1a9a102766946c0
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-18 11:00:25 +0300

    update Exercise02.java file with template class

---
+++ src/com/amigoscode/Exercise02.java
@@ -0,0 +1,10 @@
+package com.amigoscode;
+
+public class Exercise02 {
+
+    public static void task01() {
+        System.out.println("I love cats");
+        // System.out.println("I hate cats");
+    }
+
+}
