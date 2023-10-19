9ee720b1c2c98b71aa234772ec05afe6c56601e3
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-19 16:00:35 +0300

    update Main.java to run Exercise05.java tasks

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -2,6 +2,8 @@ package com.amigoscode;
 
 // import java.util.Arrays;
 
+import java.text.MessageFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,7 +21,9 @@ public class Main {
         Exercise01.task04();
         Exercise01.task05();
         Exercise01.task06();
+        System.out.println("=".repeat(30));
         System.out.println("First set of exercises is done!");
+        System.out.println("=".repeat(30));
 
         Exercise02.task01();
         Exercise02.task02();
@@ -36,7 +40,9 @@ public class Main {
         Exercise02.task13();
         Exercise02.task14();
         Exercise02.task15();
+        System.out.println("=".repeat(30));
         System.out.println("Second set of exercises is done!");
+        System.out.println("=".repeat(30));
 
         Exercise03.task01();
         Exercise03.task02();
@@ -46,13 +52,33 @@ public class Main {
         Exercise03.task06();
         Exercise03.task07();
         Exercise03.task08();
+        System.out.println("=".repeat(30));
         System.out.println("Third set of exercises is done!");
+        System.out.println("=".repeat(30));
 
         System.out.println(Exercise04.task01("Hello World!"));
         System.out.println(Exercise04.task02("   amig os cod e  "));
         String[] ex04Task03Array = {"hello", "ola", "bye", "bingo"};
         System.out.println(Exercise04.task03(ex04Task03Array));
+        System.out.println("=".repeat(30));
         System.out.println("Fourth set of exercises is done!");
+        System.out.println("=".repeat(30));
+
+        Exercise05.task01(16);
+        Exercise05.task02(45, 19);
+        Exercise05.task03(45);
+        System.out.println(MessageFormat.format(
+                "It is {0} that given numbers are equal!",
+                Exercise05.task04(45, 46)));
+        Exercise05.task05(19, 19);
+        System.out.println(Exercise05.task06("hello world!"));
+        String[] ex05Task07Array = {"hello", "ola", "bye", "bingo"};
+        System.out.println(Exercise05.task07(ex05Task07Array));
+        Exercise05.task08("   ");
+        Exercise05.task08("This is a sentence with seven words.");
+        System.out.println("=".repeat(30));
+        System.out.println("Fifth set of exercises is done!");
+        System.out.println("=".repeat(30));
         }
     }
 
