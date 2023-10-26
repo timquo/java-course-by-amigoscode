83be6e1a5d1f63870099bbccaf6ff0666e5a7fd3
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-26 17:10:18 +0300

    update Main.java to run Exercise07.java tasks

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -8,84 +8,90 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
-        Integer a = 3;
-        System.out.println(a.toString());
 
         // following line runs all the homework modules
         homework();
     }
 
     public static void homework() {
-        Exercise01.task01();
-        Exercise01.task02();
-        Exercise01.task03();
-        Exercise01.task04();
-        Exercise01.task05();
-        Exercise01.task06();
-        System.out.println("=".repeat(30));
-        System.out.println("First set of exercises is done!");
-        System.out.println("=".repeat(30));
+//        Exercise01.task01();
+//        Exercise01.task02();
+//        Exercise01.task03();
+//        Exercise01.task04();
+//        Exercise01.task05();
+//        Exercise01.task06();
+//        System.out.println("=".repeat(30));
+//        System.out.println("First set of exercises is done!");
+//        System.out.println("=".repeat(30));
+//
+//        Exercise02.task01();
+//        Exercise02.task02();
+//        Exercise02.task03();
+//        Exercise02.task04();
+//        Exercise02.task05();
+//        Exercise02.task06();
+//        Exercise02.task07();
+//        Exercise02.task08();
+//        Exercise02.task09();
+//        Exercise02.task10();
+//        Exercise02.task11();
+//        Exercise02.task12();
+//        Exercise02.task13();
+//        Exercise02.task14();
+//        Exercise02.task15();
+//        System.out.println("=".repeat(30));
+//        System.out.println("Second set of exercises is done!");
+//        System.out.println("=".repeat(30));
+//
+//        Exercise03.task01();
+//        Exercise03.task02();
+//        Exercise03.task03();
+//        Exercise03.task04();
+//        Exercise03.task05();
+//        Exercise03.task06();
+//        Exercise03.task07();
+//        Exercise03.task08();
+//        System.out.println("=".repeat(30));
+//        System.out.println("Third set of exercises is done!");
+//        System.out.println("=".repeat(30));
+//
+//        System.out.println(Exercise04.task01("Hello World!"));
+//        System.out.println(Exercise04.task02("   amig os cod e  "));
+//        String[] ex04Task03Array = {"hello", "ola", "bye", "bingo"};
+//        System.out.println(Exercise04.task03(ex04Task03Array));
+//        System.out.println("=".repeat(30));
+//        System.out.println("Fourth set of exercises is done!");
+//        System.out.println("=".repeat(30));
+//
+//        Exercise05.task01(16);
+//        Exercise05.task02(45, 19);
+//        Exercise05.task03(45);
+//        System.out.println(MessageFormat.format(
+//                "It is {0} that given numbers are equal!",
+//                Exercise05.task04(45, 46)));
+//        Exercise05.task05(19, 19);
+//        System.out.println(Exercise05.task06("hello world!"));
+//        String[] ex05Task07Array = {"hello", "ola", "bye", "bingo"};
+//        System.out.println(Exercise05.task07(ex05Task07Array));
+//        Exercise05.task08("   ");
+//        Exercise05.task08("This is a sentence with seven words.");
+//        System.out.println("=".repeat(30));
+//        System.out.println("Fifth set of exercises is done!");
+//        System.out.println("=".repeat(30));
+//
+//        Exercise06.task01();
+//        String[] ex06Task02Array = {"0", "3", "6", "23"};
+//        Exercise06.task02(ex06Task02Array);
+//        Exercise06.task03();
+//        Exercise06.task04();
+//        System.out.println("=".repeat(30));
+//        System.out.println("Sixth set of exercises is done!");
+//        System.out.println("=".repeat(30));
 
-        Exercise02.task01();
-        Exercise02.task02();
-        Exercise02.task03();
-        Exercise02.task04();
-        Exercise02.task05();
-        Exercise02.task06();
-        Exercise02.task07();
-        Exercise02.task08();
-        Exercise02.task09();
-        Exercise02.task10();
-        Exercise02.task11();
-        Exercise02.task12();
-        Exercise02.task13();
-        Exercise02.task14();
-        Exercise02.task15();
-        System.out.println("=".repeat(30));
-        System.out.println("Second set of exercises is done!");
-        System.out.println("=".repeat(30));
-
-        Exercise03.task01();
-        Exercise03.task02();
-        Exercise03.task03();
-        Exercise03.task04();
-        Exercise03.task05();
-        Exercise03.task06();
-        Exercise03.task07();
-        Exercise03.task08();
-        System.out.println("=".repeat(30));
-        System.out.println("Third set of exercises is done!");
-        System.out.println("=".repeat(30));
-
-        System.out.println(Exercise04.task01("Hello World!"));
-        System.out.println(Exercise04.task02("   amig os cod e  "));
-        String[] ex04Task03Array = {"hello", "ola", "bye", "bingo"};
-        System.out.println(Exercise04.task03(ex04Task03Array));
-        System.out.println("=".repeat(30));
-        System.out.println("Fourth set of exercises is done!");
-        System.out.println("=".repeat(30));
-
-        Exercise05.task01(16);
-        Exercise05.task02(45, 19);
-        Exercise05.task03(45);
-        System.out.println(MessageFormat.format(
-                "It is {0} that given numbers are equal!",
-                Exercise05.task04(45, 46)));
-        Exercise05.task05(19, 19);
-        System.out.println(Exercise05.task06("hello world!"));
-        String[] ex05Task07Array = {"hello", "ola", "bye", "bingo"};
-        System.out.println(Exercise05.task07(ex05Task07Array));
-        Exercise05.task08("   ");
-        Exercise05.task08("This is a sentence with seven words.");
-        System.out.println("=".repeat(30));
-        System.out.println("Fifth set of exercises is done!");
-        System.out.println("=".repeat(30));
-
-        Exercise06.task01();
-        String[] ex06Task02Array = {"0", "3", "6", "23"};
-        Exercise06.task02(ex06Task02Array);
-        Exercise06.task03();
-        Exercise06.task04();
+        Exercise07.task01();
+        Exercise07.task02();
+        Exercise07.task03();
+        Exercise07.task04();
         System.out.println("=".repeat(30));
         System.out.println("Sixth set of exercises is done!");
         System.out.println("=".repeat(30));
